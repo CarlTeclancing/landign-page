010c1d68f5b3d18db2d84d9aa4b0e41d4a1baad2
YuvCarl <carldrake969@gmail.com>
2023-07-31 11:58:43 +0100

    created and uptimized the mobile virsion of this page with js

---
+++ js/app.js
@@ -0,0 +1,48 @@
+//variables in javascript
+
+
+
+var name = "carlson"
+
+let isEven = false
+
+const pi = 22/7
+
+
+
+name = "aboki"
+
+
+//global 
+
+//local
+
+
+
+
+var x = 5
+var y = 6
+
+
+
+function changeText(){
+    var h1 = document.getElementById("h1")
+    if(h1.innerHTML == "Ui/Ux Designer And Web Developer"){
+        h1.innerHTML = "Skilled in Design and Web Developement"
+    }else{
+        h1.innerHTML = "Ui/Ux Designer And Web Developer"
+    }
+}
+
+
+let changeImg = ()=>{
+    var img = document.getElementById("img")
+
+    if(img.src.match("./assets/crestlancing.com.png")){
+        img.src = "./assets/elias62c19f5d5417f9.26929814.jpg"
+    }else{
+        img.src = "./assets/crestlancing.com.png"
+    }
+}
+
+
